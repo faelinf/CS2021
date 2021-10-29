0ebeb0dc2ac429e42fca66787ced929180ff60c1
faelinf! <rafaelcampos@discente.ufg.br>
2021-10-29 20:00:51 -0300

    23

---
+++ tarefa023/EnderecoTipo.java
@@ -0,0 +1,4 @@
+public enum EnderecoTipo {
+
+    RESIDENCIAL, COMERCIAL;
+}
---
+++ tarefa023/Estado.java
@@ -0,0 +1,12 @@
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@EqualsAndHashCode
+public class Estado {
+    
+    private String nome;
+    private Pais pais;
+}
